400ac3b9f1934ab571e80d40ad6f9b17e28d05cc
Eric Lan <tritium86@gmail.com>
2019-12-10 13:46:43 +0800

    Remove default always generate session cookie
    Session cookie does not auto generate on every request by default
    Use new_session to generate session cookie
    
    - Minor code cleanup

--- src/cowboy2_session.erl
+++ src/cowboy2_session.erl
@@ -1,9 +1,12 @@
 -module(cowboy2_session).
 
-%-- app
+%% app
 -export([start/0]).
 
-%-- api
+%% constants
+-export([ttl_default/0, cookie_name/0]).
+
+%% api
 -export([
   set/3, set/4, mset/2, mset/3
   , get/2, mget/2, get_keys/1
@@ -15,21 +18,20 @@
 
 -export([new_session/1, get_session/1]).
 
-% -behaviour(cowboy_middleware).
-%-- cowboy middleware callback
+%% -behaviour(cowboy_middleware).
+%% overload cowboy middleware callback
 -export([execute/2]).
 
-% -type session_kv() :: #{
-%     id => binary()
-%   , kv_map => map()
-%   , expire_in => integer()
-% }.
 -exported_types([session_id/0, session_key/0]).
 -type session_id() :: binary().
 -type session_key() :: any().
 
+-define(S_SESSION_KEY_NAME, <<"esessionid">>).
+
 ttl_one_day() -> 86400000.
 ttl_default() -> ttl_one_day() * 30.
+
+cookie_name() -> ?S_SESSION_KEY_NAME.
 
 start() ->
   application:ensure_all_started(?MODULE).
@@ -67,24 +69,17 @@ keys() ->
 expire(_Key) -> ok.
 touch(_Key) -> ok.
 
-%--- overload cowboy middleware callback
-%---
-execute(Req0, #{ handler_opts := Opts } = Env) ->
-	case cowboy_req:path(Req0) of
-    <<"/public">> -> {ok, Req0, Env}; % skip session for static files
-    <<"/public",_/binary>> -> {ok, Req0, Env}; % skip session for static files
-    _ ->
-      {SessId, Req1} = get_session(Req0),
-
-      % io:format("Env: ~p~n", [Env]),
-
-      Env2 = Env#{ handler_opts := [{esessionid, SessId} | Opts] },
-
-      {ok, Req1, Env2}
-
+%% overload cowboy middleware callback
+%%
+execute(Req, #{ handler_opts := Opts } = Env) ->
+  case get_session(Req) of
+    undefined -> { ok, Req, Env }
+  
+    ; {SessId, Req1} -> 
+      {ok, Req1, Env#{ handler_opts := [{esessionid, SessId} | Opts] } }
   end.
 
-%--- private
+-spec new_session(cowboy_req:req()) -> {session_id(), cowboy_req:req()}.
 new_session(Req) ->
   Id = cowboy2_session_g:gen_session_id(),
   Secret = cowboy2_session_g:get_session_secret(),
@@ -93,32 +88,29 @@ new_session(Req) ->
   LastSeen = cowboy2_session_g:unixtime(),
   cowboy2_session:set(Id, last_seen, LastSeen, ttl_default()),
 	Req1 = cowboy_req:set_resp_cookie(
-    <<"esessionid">>, EncryptedId,
+    ?S_SESSION_KEY_NAME, EncryptedId,
     Req, #{
        path => <<"/">>
-      ,max_age => ttl_default() div 1000
+      , http_only => true
+      , max_age => ttl_default() div 1000
     }),
   {Id, Req1}.
 
-%-spec get_session(Req::any()) -> {session_id(), Req::any()}.
+-spec get_session(cowboy_req:req()) -> {session_id(), cowboy_req:req()} | undefined.
 get_session(Req) ->
   MatchCookies = [{esessionid, [], <<>>}],
   case cowboy_req:match_cookies(MatchCookies, Req) of
     #{esessionid := <<>>} ->
-        % io:format("empty cookie~n"),
-        ?MODULE:new_session(Req);
+      undefined
 
-      #{esessionid := Esession} ->
+    ; #{esessionid := Esession} ->
         Secret = cowboy2_session_g:get_session_secret(),
         SessId = cowboy2_session_g:decrypt(Secret, Esession),
         % io:format("existing session: ~p~n", [SessId]),
         % check if session valid
         case cowboy2_session:get_keys(SessId) of
-          not_found -> ?MODULE:new_session(Req);
+          not_found -> undefined;
           _Keys -> {SessId, Req}
-        end;
+        end
 
-      _ ->
-        % io:format("new session ~n"),
-        ?MODULE:new_session(Req)
     end.
